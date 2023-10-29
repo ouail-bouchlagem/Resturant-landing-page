8ed057a15466df0b888f6010b643cacf5077c473
wail <117646901+tanka0@users.noreply.github.com>
2023-10-29 06:55:00 +0100

    add deply scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:  "/Resturant-landing-page/"
+  base:  "/Resturant-landing-page/",
+  plugins: [react()]
 })
